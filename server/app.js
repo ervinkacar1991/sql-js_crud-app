756cf34bb73cf45430dca1ad57aecbf751dbcbfb
ervinkacar1991 <kacarervin@gmail.com>
2022-09-04 19:40:32 +0200

    add get method to backend

---
+++ server/app.js
@@ -0,0 +1,28 @@
+const express = require("express");
+
+const app = express();
+
+const cors = require("cors");
+const dotenv = require("dotenv");
+const { response } = require("express");
+dotenv.config();
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+//create
+app.post("/insert", (request, response) => {});
+
+//read
+app.get("/getAll", (request, response) => {
+  response.json({
+    success: true,
+  });
+});
+//update
+
+//delete
+
+app.listen(process.env.PORT, () => {
+  console.log("app is running");
+});
