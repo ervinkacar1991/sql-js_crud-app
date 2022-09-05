6a9113cfe467d34e57a010fd4245ad37b03bac1f
ervinkacar1991 <kacarervin@gmail.com>
2022-09-05 11:29:13 +0200

    add database connection

--- server/app.js
+++ server/app.js
@@ -1,11 +1,12 @@
 const express = require("express");
-
 const app = express();
-
 const cors = require("cors");
 const dotenv = require("dotenv");
 const { response } = require("express");
 dotenv.config();
+
+const dbServis = require("./dvServis");
+
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
